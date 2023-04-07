ae5437e391fdd379142f57ac6307574c1f711923
jodichoo <jodi.choo@gmail.com>
2023-04-08 00:09:22 +0800

    abit less clapped

--- src/App.js
+++ src/App.js
@@ -1,50 +1,63 @@
-import './App.css'
-import { runInContext, createContext } from 'sml-slang'
-import { Variant } from 'sml-slang/dist/types'
+import "./App.css";
+import { runInContext, createContext } from "sml-slang";
+import { Variant } from "sml-slang/dist/types";
 import { useState } from "react";
 
 function App() {
-  const [userInput, setUserInput] = useState("")
-  const [progOutput, setProgOutput] = useState("Run your code!")
-  const [hasError, setHasError] = useState(false)
-  const context = createContext(Variant.DEFAULT, undefined, undefined)
+  const [userInput, setUserInput] = useState("");
+  const [progOutput, setProgOutput] = useState("Run your code!");
+  const [hasError, setHasError] = useState(false);
+  const context = createContext(Variant.DEFAULT, undefined, undefined);
   const options = {
-    scheduler: 'preemptive',
-    executionMethod: 'interpreter',
+    scheduler: "preemptive",
+    executionMethod: "interpreter",
     variant: Variant.DEFAULT,
-    useSubst: false
-  }
+    useSubst: false,
+  };
 
   function handleRun() {
-    setHasError(false)
-    runInContext(userInput, context, options).catch(
-      error => {
-        setProgOutput(error.explain())
-        setHasError(true)
-      }
-    ).then(data => {
-      if (data) {
-        setProgOutput(data.value.value)
-      }
-    })
+    setHasError(false);
+    runInContext(userInput, context, options)
+      .catch((error) => {
+        setProgOutput(error.explain());
+        setHasError(true);
+      })
+      .then((data) => {
+        if (data) {
+          setProgOutput(data.value.value);
+        }
+      });
   }
 
   return (
     <div className="App">
-      <div className='nav'> SML Slang </div>
-      <div className='container'>
+      <div className="nav"> SML Slang </div>
+      <div className="container">
         <div className="input">
           Type your Standard ML input here!
-          <input type="text" value={userInput} onChange={e => setUserInput(e.target.value)} className='input-box'/>
-          <button onClick={handleRun} className='button'>
+          <textarea
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+            rows={40}
+            cols={80}
+            autoFocus
+            className="input-box"
+          />
+          <button onClick={handleRun} className="button">
             Run
           </button>
         </div>
-        <div className='output'>
-          {hasError 
-          ? <img src="./sad-kermit.png" alt="sad kermit" />
-          : <img src="./happy-kermit.png" alt="sad kermit" />}
-          <div className='output-box'>{progOutput}</div>
+        <div className="output">
+          <div>
+            {hasError ? (
+              <img src="./sad-kermit.png" alt="sad kermit" />
+            ) : (
+              <img src="./happy-kermit.png" alt="sad kermit" />
+            )}
+          </div>
+          <div className="output-list">
+            <div className="output-box">{progOutput}</div>
+          </div>
         </div>
       </div>
     </div>
